7ba0955ab5d01edb534ac7c6c05ee56be3b52729
DUXFX <88862144+DUXFX@users.noreply.github.com>
2021-09-14 23:53:05 -0700

    Update loader.js

--- willsCheatMenu/loader.js
+++ willsCheatMenu/loader.js
@@ -1,3 +1,78 @@
-(async() => {
-	eval(await (await fetch("https://raw.githubusercontent.com/PatheticMustan/ProdigyMathGameHacking/HEAD/willsCheatMenu/dist/bundle.js")).text())
-})()
+ {
+ "createdBy": "Redirector v3.5.3",
+ "createdAt": "2020-06-04T19:08:44.799Z",
+ "redirects": [
+  {
+   "description": "game.min.js",
+   "exampleUrl": "https://code.prodigygame.com/code/2-9...",
+   "exampleResult": "https://prodigyhacking.ml/game.min.js",
+   "error": null,
+   "includePattern": "https://code.prodigygame.com/code/*/g...",
+   "excludePattern": "",
+   "patternDesc": "",
+   "redirectUrl": "https://prodigyhacking.ml/game.min.js",
+   "patternType": "W",
+   "processMatches": "noProcessing",
+   "disabled": false,
+   "grouped": false,
+   "appliesTo": ["script"]
+  },
+  {
+   "description": "Debugging - DO NOT ENABLE",
+   "exampleUrl": "https://code.prodigygame.com/code/*/g...",
+   "exampleResult": "http://localhost:1337/game.min.js",
+   "error": null,
+   "includePattern": "https://code.prodigygame.com/code/*/g...",
+   "excludePattern": "",
+   "patternDesc": "",
+   "redirectUrl": "http://localhost:1337/game.min.js",
+   "patternType": "W",
+   "processMatches": "noProcessing",
+   "disabled": true,
+   "grouped": false,
+   "appliesTo": ["script"]
+  },
+  {
+   "description": "public-game.min.js",
+   "exampleUrl": "https://code.prodigygame.com/js/publi...",
+   "exampleResult": "https://prodigy-cheat-dashboard.herok...",
+   "error": null,
+   "includePattern": "https://code.prodigygame.com/js/publi...",
+   "excludePattern": "",
+   "patternDesc": "",
+   "redirectUrl": "https://prodigyhacking.ml/public-game...",
+   "patternType": "R",
+   "processMatches": "noProcessing",
+   "disabled": false,
+   "grouped": false,
+   "appliesTo": ["script"]
+  },
+  {
+   "description": "Member Hack",
+   "exampleUrl": "https://api.prodigygame.com/game-api/...",
+   "exampleResult": "https://api.prodigygame.com/game-api/...",
+   "error": null,
+   "includePattern": "https://api.prodigygame.com/game-api/...)\\?isMember=0&userID=(.+)",
+   "excludePattern": "",
+   "patternDesc": "",
+   "redirectUrl": "https://api.prodigygame.com/game-api/...",
+   "patternType": "R",
+   "processMatches": "noProcessing",
+   "disabled": false,
+   "grouped": false,
+   "appliesTo": [
+    "sub_frame",
+    "stylesheet",
+    "script",
+    "image",
+    "imageset",
+    "object",
+    "xmlhttprequest",
+    "history",
+    "other"
+   ]
+  }
+ ]
+}
+Hacked Account: User:hacked68
+pass:hack
